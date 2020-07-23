2bc598f56baf72927d9b083edbcc509c838d1bda
Alex <snoopcom@list.ru>
2020-07-23 17:40:26 +0400

    Updates

---
+++ precache-manifest.98910e6839b01c71985b805751aff661.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d706ba66e0c72167dba32e4a384d7b4f",
+    "url": "/block-platform-form/index.html"
+  },
+  {
+    "revision": "3fff0df9590a02713596",
+    "url": "/block-platform-form/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "c809fc0c6df5714b4168",
+    "url": "/block-platform-form/static/css/main.5c33e1e3.chunk.css"
+  },
+  {
+    "revision": "3fff0df9590a02713596",
+    "url": "/block-platform-form/static/js/2.45acded2.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/block-platform-form/static/js/2.45acded2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c809fc0c6df5714b4168",
+    "url": "/block-platform-form/static/js/main.055a132b.chunk.js"
+  },
+  {
+    "revision": "bcb3ed69bb8e0b5dc91a",
+    "url": "/block-platform-form/static/js/runtime-main.518ec81a.js"
+  }
+]);
