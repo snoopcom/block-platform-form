a502a59e74a8d6b971fb1c59ec29aa44d9b7b687
Alex <snoopcom@list.ru>
2020-08-03 11:37:12 +0400

    final

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,30 +1,3 @@
-// import axios from 'axios';
-
-/* запрос на регистрацию
-export const signUp = async ({ username, email, password }) => {
-  try {
-    await axios.post('/users', {
-      user: {
-        username,
-        email,
-        password,
-      },
-    });
-  } catch (error) {
-    alert('Такой пользователь существует');
-  }
-};
- */
-
-/* запрос на авторизацию
-export const login = ({ email, password }) => axios.post('/users/login', {
-  user: {
-    email,
-    password,
-  },
-});
-*/
-
 export const logInAction = (email, password) => ({
   type: 'LOGIN',
   profile: { email, password },
