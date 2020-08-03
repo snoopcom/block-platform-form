ba9d5868c7152cb78aa4c5e3ef2efd25bb95df9b
Alex <snoopcom@list.ru>
2020-08-03 13:56:43 +0400

    final

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,8 +6,6 @@ const reducer = (state = initState, action) => {
   switch (action.type) {
     case 'LOGIN':
       return { ...state, ...action.profile, login: true };
-    case 'SIGNUP':
-      return { ...state, ...action.profile };
     case 'LOGOUT':
       return { ...state, login: false };
     default:
