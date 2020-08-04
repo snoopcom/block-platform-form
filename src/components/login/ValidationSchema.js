5052c2387f3d5f1eafb4d68e05260a27eac5e15b
Alex <snoopcom@bk.ru>
2020-08-04 23:46:01 +0300

    final

--- src/components/login/ValidationSchema.js
+++ src/components/login/ValidationSchema.js
@@ -2,12 +2,6 @@ import * as Yup from 'yup';
 
 const validationSchema = Yup.object({
   email: Yup.string().email('Неккоретный email').required('Пожалуйста, введите почту'),
-  password: Yup.string()
-    .matches(
-      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=(.*[a-zA-Z]){4}).{8,20}$/,
-      'Введите от 8 до 40 символов, как минимум одна цифра и одна заглавная буква',
-    )
-    .required('Пожалуйста, введите пароль'),
 });
 
 export default validationSchema;
